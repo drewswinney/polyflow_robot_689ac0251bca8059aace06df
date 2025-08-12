732ea6e52bb50246d19cbdf65068d0eb59b8a84f
Drew Swinney <drewswinney@gmail.com>
2025-08-12 00:16:43 -0400

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,81 @@
+{ config, pkgs, lib, ... }:
+
+let
+  user = "admin";
+  password = "password";
+  interface = "end0";
+  hostname = "689ac0251bca8059aace06df";
+in {
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
+  imports = [
+    "${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git";}}/raspberry-pi/4"
+  ];
+  
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  system.autoUpgrade.flags = ["--max-jobs" "1" "--cores" "1"];
+
+  networking = {
+    hostName = "689ac0251bca8059aace06df";
+    networkmanager.enable = true;
+    nftables.enable = true;
+  };
+
+  environment.etc."nixos/configuration.nix" = {
+    source = ./configuration.nix;
+    mode = "0644";
+  };
+
+  environment.systemPackages = with pkgs; with rosPackages.humble; [ vim git wget inetutils ros-base ];
+
+  services.openssh.enable = true;
+
+  users = {
+    mutableUsers = false;
+    users."${user}" = {
+      isNormalUser = true;
+      password = password;
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  # Services
+  systemd.services.git-clone = {
+    description = "Clone the robot git repository on startup";
+    wantedBy = [ "multi-user.target" ]; # Or a more specific target if needed
+    after = [ "network-online.target" ]; # Ensure network is available
+    script = ''
+      ${pkgs.git}/bin/git config --global --unset https.proxy
+      ${pkgs.git}/bin/git clone https://github.com/drewswinney/polyflow_robot_689ac0251bca8059aace06df.git ~/home/admin
+      chown -R ${user}:${user} ~/home/admin/polyflow_robot_689ac0251bca8059aace06df
+    '';
+  };
+
+  services.vscode-server.enable = true;
+
+  nix.settings.experimental-features = ["nix-command" "flakes" ];
+
+  hardware.enableRedistributableFirmware = true;
+  system.stateVersion = "23.11";
+}
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    # Ensure nixpkgs follows nix-ros-overlay's version to avoid compatibility issues
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs"; 
+  };
+
+  outputs = { self, nixpkgs, nix-ros-overlay, vscode-server, ... }@inputs:
+    let 
+      pkgsOverride = (inputs: {
+        nixpkgs = {
+          config.allowUnfree = true;
+            overlays = [
+              nix-ros-overlay.overlays.default
+            ];
+          };
+        });
+    in { 
+      nixosConfigurations."689ac0251bca8059aace06df" = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          modules = [
+            # Base NixOS modules
+            ./configuration.nix 
+            vscode-server.nixosModules.default
+            # Add the nix-ros-overlay to your system overlays
+            { nixpkgs.overlays = [ nix-ros-overlay.overlays.default ]; }
+            # You may also need to include nixos-hardware for specific Raspberry Pi 4 hardware support
+            # nixos-hardware.nixosModules.raspberry-pi-4 
+          ];
+          # Further configuration specific to your Raspberry Pi and ROS needs
+        };
+        substituters = https://ros.cachix.org;
+        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=;
+    };
+}
